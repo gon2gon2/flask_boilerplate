c1654c3989673e8f48533c9c1afb01ec093b1e3c
gon2gon2 <ark5354@gmail.com>
2021-09-15 11:53:50 +0900

    linux

--- project_name/models.py
+++ project_name/models.py
@@ -7,12 +7,3 @@ class Test(db.Model):
     
     def __init__(self, datetime):
         self.datetime = datetime
-        
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.String(10))
-    pw = db.Column(db.String(30))
-    
-    def __init__(self, user_id, pw):
-        self.user_id = user_id
-        self.pw = pw
--- project_name/views/main_views.py
+++ project_name/views/main_views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, request, session, render_template
+from flask import Blueprint, render_template
 
 from project_name import db
-from ..models import Test, User
+from ..models import Test
 
 from datetime import datetime
 
@@ -16,15 +16,3 @@ def home():
     db.session.commit()
     
     return render_template('mainpage.html')
-
-@bp.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method=='GET':
-        return render_template('signup.html')
-    
-    id = request.form['id']
-    pw = request.form['pw']
-    user = User(user_id=id, pw=pw)
-    db.session.add(user)
-    db.session.commit()
-    return render_template('signup.html')
