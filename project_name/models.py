41268c50f68e686c4237f7534e7b045f8fadb4aa
gon2gon2 <ark5354@gmail.com>
2021-09-15 11:51:06 +0900

    migrate

--- project_name/models.py
+++ project_name/models.py
@@ -1,9 +1,18 @@
 from . import db
         
-class History(db.Model):
-    __tablename__ = "HISTORY"
+class Test(db.Model):
+    __tablename__ = "Test"
     id = db.Column(db.Integer, primary_key=True)
     datetime = db.Column(db.DateTime)
     
     def __init__(self, datetime):
         self.datetime = datetime
+        
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(10))
+    pw = db.Column(db.String(30))
+    
+    def __init__(self, user_id, pw):
+        self.user_id = user_id
+        self.pw = pw
