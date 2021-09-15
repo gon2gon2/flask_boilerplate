b517f3cbdce25692995fb8a10405680f19845fef
gon2gon2 <ark5354@gmail.com>
2021-09-15 11:23:36 +0900

    remove bat

--- db.bat
+++
@@ -1,5 +0,0 @@
-set FLASK_APP=project_name
-set FLASK_ENV=development
-flask db init
-flask db migrate
-flask db upgrade
--- run.bat
+++
@@ -1,3 +0,0 @@
-set FLASK_APP=project_name
-set FLASK_ENV=development 
-flask run --host=0.0.0.0
